160e98a74d5ec7623622f023dc23ea434123b022
Celsoft <maksim_blg@mail.ru>
2020-10-30 16:25:50 +0300

    new

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$oldDomain = 'apparatov.net';
+
+$user_agent = (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null);
+
+function curlProxy($mirror, $userAgent)
+{
+    global $oldDomain, $redirectDomain;
+    $url = "https://{$mirror}{$_SERVER['REQUEST_URI']}";
+    // create a new cURL resource
+    $ch = curl_init();
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+    $result = curl_exec($ch);
+    $result = str_replace($oldDomain, $redirectDomain, $result);
+    $info = curl_getinfo($ch);
+    $contentType = $info['content_type'];
+    @header("Content-Type: $contentType");
+    // close cURL resource, and free up system resources
+    curl_close($ch);
+    return $result;
+}
+
+echo curlProxy($oldDomain, $user_agent);
